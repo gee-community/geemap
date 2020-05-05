5ed25415ea5e586c4f4a3739e93f4b8409389907
Oliver <oliverburdekin@burdgis.com>
2020-05-05 11:02:49 +0100

    Change function name to sentinel2_timseries() and tidy up

--- geemap/geemap.py
+++ geemap/geemap.py
@@ -3141,7 +3141,22 @@ def naip_timeseries(roi=None, start_year=2009, end_year=2018):
     except Exception as e:
         print(e)
 
-def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01-01', end_date='12-31'):
+def sentinel2_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01-01', end_date='12-31'):
+    """Generates an annual Sentinel 2 ImageCollection. This algorithm is adapted from https://gist.github.com/jdbcode/76b9ac49faf51627ebd3ff988e10adbc. A huge thank you to Justin Braaten for sharing his fantastic work.
+       Images include both level 1C and level 2A imagery.
+    Args:
+
+        roi (object, optional): Region of interest to create the timelapse. Defaults to None.
+        start_year (int, optional): Starting year for the timelapse. Defaults to 2015.
+        end_year (int, optional): Ending year for the timelapse. Defaults to 2019.
+        start_date (str, optional): Starting date (month-day) each year for filtering ImageCollection. Defaults to '01-01'.
+        end_date (str, optional): Ending date (month-day) each year for filtering ImageCollection. Defaults to '12-31'.
+    Returns:
+        object: Returns an ImageCollection containing annual Sentinel 2 images.
+    """
+
+    ################################################################################
+
     ################################################################################
     # Input and output parameters.
     import re
@@ -3216,14 +3231,7 @@ def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01
     def colFilter(col, aoi):  # , startDate, endDate):
         return(col.filterBounds(aoi))
 
-    # Landsat collection preprocessingEnabled
-    # Get Landsat surface reflectance collections for OLI, ETM+ and TM sensors.
-    # LC08col = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
-    # LE07col = ee.ImageCollection('LANDSAT/LE07/C01/T1_SR')
-    # LT05col = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR')
-    # LT04col = ee.ImageCollection('LANDSAT/LT04/C01/T1_SR')
-
-    # Get Sentinel 2 surface reflectance collections for MSI, Level-2A
+    # Get Sentinel 2 collections, both Level-1C (top of atmophere) and Level-2A (surface reflectance)
     MSILCcol = ee.ImageCollection('COPERNICUS/S2')
     MSI2Acol = ee.ImageCollection('COPERNICUS/S2_SR')
 
@@ -3236,19 +3244,6 @@ def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01
         # .filter('GEOMETRIC_RMSE_MODEL < 15')
         # .filter('IMAGE_QUALITY == 9 || IMAGE_QUALITY_OLI == 9'))
 
-    # Function to get and rename bands of interest from OLI.
-    # def renameOli(img):
-    #     return(img.select(
-    #         ['B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'pixel_qa'],
-    #         ['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2', 'pixel_qa']))
-
-    # Function to get and rename bands of interest from ETM+.
-    # def renameEtm(img):
-    #     return(img.select(
-    #         ['B1', 'B2', 'B3', 'B4', 'B5', 'B7', 'pixel_qa'],
-    #         ['Blue', 'Green', 'Red', 'NIR', 'SWIR1', 'SWIR2', 'pixel_qa']))
-
-    '''>>Double check quality (cloud shadow from SLC)'''
     # Function to get and rename bands of interest from MSI
     def renameMSI(img):
         return(img.select(
@@ -3263,16 +3258,7 @@ def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01
 
     # Define function to mask out clouds and cloud shadows in images.
     # Use CFmask band included in USGS Landsat SR image product.
-    # def fmask(img):
-    #     cloudShadowBitMask = 1 << 3
-    #     cloudsBitMask = 1 << 5
-    #     qa = img.select('pixel_qa')
-    #     mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0) \
-    #         .And(qa.bitwiseAnd(cloudsBitMask).eq(0))
-    #     return(img.updateMask(mask))
 
-    # Define function to mask out clouds and cloud shadows in images.
-    # Use CFmask band included in USGS Landsat SR image product.
     def fmask(img):
         cloudOpaqueBitMask = 1 << 10
         cloudCirrusBitMask = 1 << 11
@@ -3281,22 +3267,6 @@ def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01
             .And(qa.bitwiseAnd(cloudCirrusBitMask).eq(0))
         return(img.updateMask(mask))
 
-    # Define function to prepare OLI images.
-    # def prepOli(img):
-    #     orig = img
-    #     img = renameOli(img)
-    #     img = fmask(img)
-    #     return (ee.Image(img.copyProperties(orig, orig.propertyNames()))
-    #             .resample('bicubic'))
-
-    # Define function to prepare ETM+ images.
-    # def prepEtm(img):
-    #     orig = img
-    #     img = renameEtm(img)
-    #     img = fmask(img)
-    #     return(ee.Image(img.copyProperties(orig, orig.propertyNames()))
-    #            .resample('bicubic'))
-
     # Define function to prepare MSI images.
     def prepMSI(img):
         orig = img
@@ -3304,8 +3274,7 @@ def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01
         img = fmask(img)
         return(ee.Image(img.copyProperties(orig, orig.propertyNames()))
                 .resample('bicubic'))
-
-
+    
     # Get annual median collection.
     def getAnnualComp(y):
         startDate = ee.Date.fromYMD(
@@ -3313,15 +3282,10 @@ def sentinel_timeseries(roi=None, start_year=2015, end_year=2019, start_date='01
         endDate = startDate.advance(ee.Number(n_days), 'day')
 
         # Filter collections and prepare them for merging.
-        # LC08coly = colFilter(LC08col, roi, startDate, endDate).map(prepOli)
-        # LE07coly = colFilter(LE07col, roi, startDate, endDate).map(prepEtm)
-        # LT05coly = colFilter(LT05col, roi, startDate, endDate).map(prepEtm)
-        # LT04coly = colFilter(LT04col, roi, startDate, endDate).map(prepEtm)
         MSILCcoly = colFilter(MSILCcol, roi, startDate, endDate).map(prepMSI)
         MSI2Acoly = colFilter(MSI2Acol, roi, startDate, endDate).map(prepMSI)
 
         # Merge the collections.
-        #col = LC08coly.merge(LE07coly).merge(LT05coly).merge(LT04coly)
         col = MSILCcoly.merge(MSI2Acoly)
 
         yearImg = col.median()
@@ -3426,7 +3390,7 @@ def landsat_timeseries(roi=None, start_year=1984, end_year=2019, start_date='06-
     if re.match("[0-9]{2}\-[0-9]{2}", start_date) and re.match("[0-9]{2}\-[0-9]{2}", end_date):
         pass
     else:
-        print('The start data and end date must be month-day, such as 06-10, 09-20')
+        print('The start date and end date must be month-day, such as 06-10, 09-20')
         return
 
     try:
