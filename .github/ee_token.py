151192a9176a0f82eda07d003336508ca8bb1563
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2020-05-06 10:26:51 -0500

    GH action added

---
+++ .github/ee_token.py
@@ -0,0 +1,7 @@
+import os
+ee_token = os.environ['EARTHENGINE_TOKEN']
+credential = '{"refresh_token":"%s"}' % ee_token
+credential_file_path = os.path.expanduser("~/.config/earthengine/")
+os.makedirs(credential_file_path,exist_ok=True)
+with open(credential_file_path + 'credentials', 'w') as file:
+    file.write(credential)
