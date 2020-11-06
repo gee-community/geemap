bfdff5cbc57050731c46228b910e7c6b013f4059
Qiusheng Wu <giswqs@gmail.com>
2020-11-06 17:42:36 -0500

    Auto format using black

--- setup.py
+++ setup.py
@@ -6,63 +6,65 @@ from os import path as op
 import io
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 here = op.abspath(op.dirname(__file__))
 
 # get the dependencies and installs
-with io.open(op.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
-    if platform.system() == 'Windows':
-        all_reqs.append('pywin32')
+with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
+    all_reqs = f.read().split("\n")
+    if platform.system() == "Windows":
+        all_reqs.append("pywin32")
 
-install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
-dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
+install_requires = [x.strip() for x in all_reqs if "git+" not in x]
+dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
 
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    "Click>=7.0",
+]
 
-setup_requirements = [ ]
+setup_requirements = []
 
-test_requirements = [ ]
+test_requirements = []
 
 setup(
     author="Qiusheng Wu",
-    author_email='giswqs@gmail.com',
-    python_requires='>=3.5',
+    author_email="giswqs@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="A Python package for interactive mapping using Google Earth Engine and ipyleaflet",
     entry_points={
-        'console_scripts': [
-            'geemap=geemap.cli:main',
+        "console_scripts": [
+            "geemap=geemap.cli:main",
         ],
     },
     install_requires=install_requires,
     dependency_links=dependency_links,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='geemap',
-    name='geemap',
-    packages=find_packages(include=['geemap', 'geemap.*']),
+    keywords="geemap",
+    name="geemap",
+    packages=find_packages(include=["geemap", "geemap.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/giswqs/geemap',
-    version='0.8.1',
+    url="https://github.com/giswqs/geemap",
+    version="0.8.1",
     zip_safe=False,
 )
