07223b26e72e5072553f37a40c8bed352e60c69b
Qiusheng Wu <giswqs@gmail.com>
2020-12-21 16:03:33 -0500

    Bump version: 0.8.5 → 0.8.6

--- geemap/__init__.py
+++ geemap/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "0.8.5"
+__version__ = "0.8.6"
 
 
 def in_colab_shell():
--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/geemap",
-    version="0.8.5",
+    version="0.8.6",
     zip_safe=False,
 )
