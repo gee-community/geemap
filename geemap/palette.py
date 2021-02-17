761f89ecd0645ec7bfb0d0cc2c43ff97893e9c8e
Qiusheng Wu <giswqs@gmail.com>
2021-02-16 20:33:07 -0500

    Added a palette module #302

---
+++ geemap/palette.py
@@ -0,0 +1,38 @@
+"""Module for commonly used palettes for visualizing Earth Engine data.
+"""
+
+
+class Palette:
+
+    ndvi = [
+        "FFFFFF",
+        "CE7E45",
+        "DF923D",
+        "F1B555",
+        "FCD163",
+        "99B718",
+        "74A901",
+        "66A000",
+        "529400",
+        "3E8601",
+        "207401",
+        "056201",
+        "004C00",
+        "023B01",
+        "012E01",
+        "011D01",
+        "011301",
+    ]
+
+    ndwi = [
+        "#ece7f2",
+        "#d0d1e6",
+        "#a6bddb",
+        "#74a9cf",
+        "#3690c0",
+        "#0570b0",
+        "#045a8d",
+        "#023858",
+    ]
+
+    dem = ["006633", "E5FFCC", "662A00", "D8D8D8", "F5F5F5"]
