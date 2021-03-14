c6f4fd3cb9ac69a0579321fc49f63516b841d859
Qiusheng Wu <giswqs@gmail.com>
2021-03-14 15:39:58 -0400

    Added tool gui template #239

--- geemap/toolbar.py
+++ geemap/toolbar.py
@@ -2,11 +2,183 @@
 """
 import ee
 import os
+import ipyevents
 import ipywidgets as widgets
 from ipyleaflet import WidgetControl, DrawControl
 from IPython.core.display import display
 from ipyfilechooser import FileChooser
 from .common import *
+
+
+def tool_template(m):
+
+    widget_width = "250px"
+    padding = "0px 0px 0px 5px"  # upper, right, bottom, left
+
+    toolbar_button = widgets.ToggleButton(
+        value=False,
+        tooltip="Toolbar",
+        icon="gear",
+        layout=widgets.Layout(width="28px", height="28px", padding="0px 0px 0px 4px"),
+    )
+
+    close_button = widgets.ToggleButton(
+        value=False,
+        tooltip="Close the tool",
+        icon="times",
+        button_style="primary",
+        layout=widgets.Layout(height="28px", width="28px", padding="0px 0px 0px 4px"),
+    )
+
+    checkbox = widgets.Checkbox(
+        description="Checkbox",
+        indent=False,
+        layout=widgets.Layout(padding=padding, width=widget_width),
+    )
+
+    dropdown = widgets.Dropdown(
+        options=["Option 1", "Option 2", "Option 3"],
+        value=None,
+        description="Dropdown:",
+        layout=widgets.Layout(width=widget_width, padding=padding),
+        style={"description_width": "initial"},
+    )
+
+    int_slider = widgets.IntSlider(
+        min=1,
+        max=100,
+        description="Int Slider: ",
+        readout=False,
+        continuous_update=True,
+        layout=widgets.Layout(width="220px", padding=padding),
+        style={"description_width": "initial"},
+    )
+
+    int_slider_label = widgets.Label()
+    widgets.jslink((int_slider, "value"), (int_slider_label, "value"))
+
+    float_slider = widgets.FloatSlider(
+        min=1,
+        max=100,
+        description="Float Slider: ",
+        readout=False,
+        continuous_update=True,
+        layout=widgets.Layout(width="220px", padding=padding),
+        style={"description_width": "initial"},
+    )
+
+    float_slider_label = widgets.Label()
+    widgets.jslink((float_slider, "value"), (float_slider_label, "value"))
+
+    color = widgets.ColorPicker(
+        concise=False,
+        description="Color:",
+        value="white",
+        style={"description_width": "initial"},
+        layout=widgets.Layout(width=widget_width, padding=padding),
+    )
+
+    text = widgets.Text(
+        value="",
+        description="Textbox:",
+        placeholder="Placeholder",
+        style={"description_width": "initial"},
+        layout=widgets.Layout(width=widget_width, padding=padding),
+    )
+
+    textarea = widgets.Textarea(
+        placeholder="Placeholder",
+        layout=widgets.Layout(width=widget_width),
+    )
+
+    buttons = widgets.ToggleButtons(
+        value=None,
+        options=["Apply", "Reset", "Close"],
+        tooltips=["Apply", "Reset", "Close"],
+        button_style="primary",
+    )
+    buttons.style.button_width = "80px"
+
+    output = widgets.Output(layout=widgets.Layout(width=widget_width, padding=padding))
+
+    toolbar_widget = widgets.VBox()
+    toolbar_widget.children = [toolbar_button]
+    toolbar_header = widgets.HBox()
+    toolbar_header.children = [close_button, toolbar_button]
+    toolbar_footer = widgets.VBox()
+    toolbar_footer.children = [
+        checkbox,
+        widgets.HBox([int_slider, int_slider_label]),
+        widgets.HBox([float_slider, float_slider_label]),
+        dropdown,
+        text,
+        color,
+        textarea,
+        buttons,
+        output,
+    ]
+
+    toolbar_event = ipyevents.Event(
+        source=toolbar_widget, watched_events=["mouseenter", "mouseleave"]
+    )
+
+    def handle_toolbar_event(event):
+
+        if event["type"] == "mouseenter":
+            toolbar_widget.children = [toolbar_header, toolbar_footer]
+        elif event["type"] == "mouseleave":
+            if not toolbar_button.value:
+                toolbar_widget.children = [toolbar_button]
+                toolbar_button.value = False
+                close_button.value = False
+
+    toolbar_event.on_dom_event(handle_toolbar_event)
+
+    def toolbar_btn_click(change):
+        if change["new"]:
+            close_button.value = False
+            toolbar_widget.children = [toolbar_header, toolbar_footer]
+        else:
+            if not close_button.value:
+                toolbar_widget.children = [toolbar_button]
+
+    toolbar_button.observe(toolbar_btn_click, "value")
+
+    def close_btn_click(change):
+        if change["new"]:
+            toolbar_button.value = False
+            if m.tool_control is not None and m.tool_control in m.controls:
+                m.remove_control(m.tool_control)
+                m.tool_control = None
+                toolbar_widget.close()
+
+    close_button.observe(close_btn_click, "value")
+
+    def button_clicked(change):
+        if change["new"] == "Apply":
+            with output:
+                output.clear_output()
+                print("Running ...")
+        elif change["new"] == "Reset":
+            textarea.value = ""
+            output.clear_output()
+        elif change["new"] == "Close":
+            m.toolbar_reset()
+            if m.tool_control is not None and m.tool_control in m.controls:
+                m.remove_control(m.tool_control)
+                m.tool_control = None
+                toolbar_widget.close()
+        buttons.value = None
+
+    buttons.observe(button_clicked, "value")
+
+    toolbar_control = WidgetControl(widget=toolbar_widget, position="topright")
+
+    if toolbar_control not in m.controls:
+        m.add_control(toolbar_control)
+        m.tool_control = toolbar_control
+
+    toolbar_button.value = True
 
 
 def open_data_widget(m):
