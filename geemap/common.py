90ecacebe6b7b4ddefc2269106013feb7ab906f9
Qiusheng Wu <giswqs@gmail.com>
2021-05-29 14:00:47 -0400

    Added add_osm function #503

--- geemap/common.py
+++ geemap/common.py
@@ -7668,7 +7668,7 @@ def random_sampling(
         return points
 
 
-def geocode_to_gdf(
+def osm_to_geopandas(
     query,
     which_result=None,
     by_osmid=False,
@@ -7714,6 +7714,22 @@ def osm_to_ee(
     Returns:
         ee.FeatureCollection: An Earth Engine FeatureCollection.
     """
-    gdf = geocode_to_gdf(query, which_result, by_osmid, buffer_dist)
+    gdf = osm_to_geopandas(query, which_result, by_osmid, buffer_dist)
     fc = geopandas_to_ee(gdf, geodesic)
     return fc
+
+
+def osm_to_geojson(query, which_result=None, by_osmid=False, buffer_dist=None):
+    """Retrieves place(s) by name or ID from the Nominatim API as an ee.FeatureCollection.
+
+    Args:
+        query (str | dict | list): Query string(s) or structured dict(s) to geocode.
+        which_result (INT, optional): Which geocoding result to use. if None, auto-select the first (Multi)Polygon or raise an error if OSM doesn't return one. to get the top match regardless of geometry type, set which_result=1. Defaults to None.
+        by_osmid (bool, optional): If True, handle query as an OSM ID for lookup rather than text search. Defaults to False.
+        buffer_dist (float, optional): Distance to buffer around the place geometry, in meters. Defaults to None.
+
+    Returns:
+        ee.FeatureCollection: An Earth Engine FeatureCollection.
+    """
+    gdf = osm_to_geopandas(query, which_result, by_osmid, buffer_dist)
+    return gdf.__geo_interface__
