9a8cf452c4d9b0a9d0dbea572f6fdc4d2b809cbc
Qiusheng Wu <giswqs@gmail.com>
2021-10-21 22:43:42 -0400

    Remove py36 due to xyzservices

--- setup.py
+++ setup.py
@@ -35,17 +35,14 @@ test_requirements = []
 setup(
     author="Qiusheng Wu",
     author_email="giswqs@gmail.com",
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="A Python package for interactive mapping using Google Earth Engine and ipyleaflet",
     entry_points={
