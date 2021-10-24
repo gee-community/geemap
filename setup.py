3740d1c48f0ece151ae5e5acfe13232a77516fca
Qiusheng Wu <giswqs@gmail.com>
2021-10-23 20:27:04 -0400

    Bump version: 0.9.3 → 0.9.4

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/geemap",
-    version="0.9.3",
+    version="0.9.4",
     zip_safe=False,
 )
