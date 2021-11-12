76c577421a7d71a9fac31126ee76796cf87d2b1e
Qiusheng Wu <giswqs@gmail.com>
2021-11-11 21:35:24 -0500

    Improved date sequence

--- geemap/common.py
+++ geemap/common.py
@@ -3706,17 +3706,41 @@ def date_sequence(start, end, unit, date_format="YYYY-MM-dd"):
     Args:
         start (str): The start date, e.g., '2000-01-01'.
         end (str): The end date, e.g., '2000-12-31'.
-        unit (str): One of 'year', 'month' 'week', 'day', 'hour', 'minute', or 'second'.
+        unit (str): One of 'year', 'quarter', 'month' 'week', 'day', 'hour', 'minute', or 'second'.
         date_format (str, optional): A pattern, as described at http://joda-time.sourceforge.net/apidocs/org/joda/time/format/DateTimeFormat.html. Defaults to 'YYYY-MM-dd'.
 
     Returns:
         ee.List: A list of date sequence.
     """
+
+    def get_quarter(d):
+        return str((int(d[5:7]) - 1) // 3 * 3 + 1).zfill(2)
+
+    if unit == "year":
+        start = start[:4] + "-01-01"
+    elif unit == "month":
+        start = start[:7] + "-01"
+    elif unit == "quarter":
+        start = start[:5] + get_quarter(start) + "-01"
     start_date = ee.Date(start)
     end_date = ee.Date(end)
-    count = ee.Number(end_date.difference(start_date, unit)).toInt()
-    num_seq = ee.List.sequence(0, count)
-    date_seq = num_seq.map(lambda d: start_date.advance(d, unit).format(date_format))
+
+    if unit != "quarter":
+        count = ee.Number(end_date.difference(start_date, unit)).toInt()
+        num_seq = ee.List.sequence(0, count)
+        date_seq = num_seq.map(
+            lambda d: start_date.advance(d, unit).format(date_format)
+        )
+
+    else:
+        unit = "month"
+        count = ee.Number(end_date.difference(start_date, unit)).divide(3).toInt()
+        print(count.getInfo())
+        num_seq = ee.List.sequence(0, count.multiply(3), 3)
+        date_seq = num_seq.map(
+            lambda d: start_date.advance(d, unit).format(date_format)
+        )
+
     return date_seq
 
 
