56bfc5d714b611f56b6d1d878a3a758aea9bab14
Qiusheng Wu <giswqs@gmail.com>
2021-11-22 15:01:05 -0500

    Bump version: 0.9.4 → 0.9.5

--- geemap/__init__.py
+++ geemap/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 
 import os
 
--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/giswqs/geemap",
-    version="0.9.4",
+    version="0.9.5",
     zip_safe=False,
 )
