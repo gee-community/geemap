60fe8ead0d0c246e3e6a348eb28cbbd4929eeb7e
Rambaud Pierrick <12rambau@users.noreply.github.com>
2022-05-25 17:00:19 +0200

    use values and items to loop in dicts
    I decided to use values and items method from dictionaries to loop inside them. 
    First advantage is to reduce the length of the call and remove some useless lines
    Second advantage is that it's clearer what `key` and `item` represent in the dict

--- geemap/basemaps.py
+++ geemap/basemaps.py
@@ -182,8 +182,8 @@ def get_xyz_dict(free_only=True):
                 xyz_dict[name] = tile
 
         except Exception:
-            for sub_item in item:
-                name = item[sub_item]["name"]
+            for sub_item in item.values():
+                name = sub_item["name"]
                 tile = eval("xyz." + name)
                 if eval("xyz." + name + ".requires_token()"):
                     if free_only:
@@ -205,41 +205,30 @@ def xyz_to_leaflet():
     """
     leaflet_dict = {}
 
-    for key in xyz_tiles:
-        name = xyz_tiles[key]["name"]
-        url = xyz_tiles[key]["url"]
-        attribution = xyz_tiles[key]["attribution"]
+    for key, tile in xyz_tiles.items():
+        name = tile["name"]
+        url = tile["url"]
+        attribution = tile["attribution"]
         leaflet_dict[key] = ipyleaflet.TileLayer(
             url=url, name=name, attribution=attribution, max_zoom=22
         )
 
-    for key in wms_tiles:
-        name = wms_tiles[key]["name"]
-        url = wms_tiles[key]["url"]
-        layers = wms_tiles[key]["layers"]
-        fmt = wms_tiles[key]["format"]
-        transparent = wms_tiles[key]["transparent"]
-        attribution = wms_tiles[key]["attribution"]
+    for key, tile in wms_tiles.items():
         leaflet_dict[key] = ipyleaflet.WMSLayer(
-            url=url,
-            layers=layers,
-            name=name,
-            attribution=attribution,
-            format=fmt,
-            transparent=transparent,
+            url=tile["url"],
+            layers=tile["layers"],
+            name=tile["name"],
+            attribution=tile["attribution"],
+            format=tile["format"],
+            transparent=tile["transparent"],
         )
-
-    xyz_dict = get_xyz_dict()
-    for item in xyz_dict:
-        name = xyz_dict[item].name
-        url = xyz_dict[item].build_url()
-        attribution = xyz_dict[item].attribution
-        if "max_zoom" in xyz_dict[item].keys():
-            max_zoom = xyz_dict[item]["max_zoom"]
-        else:
-            max_zoom = 22
-        leaflet_dict[name] = ipyleaflet.TileLayer(
-            url=url, name=name, max_zoom=max_zoom, attribution=attribution
+        
+    for item in get_xyz_dict().values():
+        leaflet_dict[item.name] = ipyleaflet.TileLayer(
+            url=item.build_url(), 
+            name=item.name, 
+            max_zoom=item.pop("max_zoom", 22), 
+            attribution=item.attribution
         )
 
     if os.environ.get("PLANET_API_KEY") is not None:
@@ -262,20 +251,19 @@ def xyz_to_pydeck():
 
     pydeck_dict = {}
 
-    for key in xyz_tiles:
-        url = xyz_tiles[key]["url"]
+    for key, tile in xyz_tiles.items():
+        url = item["url"]
         pydeck_dict[key] = url
 
-    xyz_dict = get_xyz_dict()
-    for item in xyz_dict:
-        url = xyz_dict[item].build_url()
-        pydeck_dict[item] = url
+    for key, item in get_xyz_dict().items():
+        url = item.build_url()
+        pydeck_dict[key] = url
 
         if os.environ.get("PLANET_API_KEY") is not None:
 
             planet_dict = planet_tiles(tile_format="ipyleaflet")
-            for tile in planet_dict:
-                pydeck_dict[tile] = planet_dict[tile].url
+            for id_, tile in planet_dict.items():
+                pydeck_dict[id_] = tile.url
 
     pdk.settings.custom_libraries = [
         {
@@ -298,51 +286,34 @@ def xyz_to_folium():
     """
     folium_dict = {}
 
-    for key in xyz_tiles:
-        name = xyz_tiles[key]["name"]
-        url = xyz_tiles[key]["url"]
-        attribution = xyz_tiles[key]["attribution"]
+    for key, tile in xyz_tiles.items():
         folium_dict[key] = folium.TileLayer(
-            tiles=url,
-            attr=attribution,
-            name=name,
+            tiles=tile["url"],
+            attr=tile["attribution"],
+            name=tile["name"],
             overlay=True,
             control=True,
             max_zoom=22,
         )
 
-    for key in wms_tiles:
-        name = wms_tiles[key]["name"]
-        url = wms_tiles[key]["url"]
-        layers = wms_tiles[key]["layers"]
-        fmt = wms_tiles[key]["format"]
-        transparent = wms_tiles[key]["transparent"]
-        attribution = wms_tiles[key]["attribution"]
+    for key, tile in wms_tiles.items():
         folium_dict[key] = folium.WmsTileLayer(
-            url=url,
-            layers=layers,
-            name=name,
-            attr=attribution,
-            fmt=fmt,
-            transparent=transparent,
+            url=tile["url"],
+            layers=tile["layers"],
+            name=tile["name"],
+            attr=tile["attribution"],
+            fmt=tile["format"],
+            transparent=tile["transparent"],
             overlay=True,
             control=True,
         )
-
-    xyz_dict = get_xyz_dict()
-    for item in xyz_dict:
-        name = xyz_dict[item].name
-        url = xyz_dict[item].build_url()
-        attribution = xyz_dict[item].attribution
-        if "max_zoom" in xyz_dict[item].keys():
-            max_zoom = xyz_dict[item]["max_zoom"]
-        else:
-            max_zoom = 22
-        folium_dict[name] = folium.TileLayer(
-            tiles=url,
-            attr=attribution,
-            name=name,
-            max_zoom=max_zoom,
+ 
+    for item in get_xyz_dict().values():
+        folium_dict[item.name] = folium.TileLayer(
+            tiles=item.build_url(),
+            attr=item.attribution,
+            name=item.name,
+            max_zoom=item.pop("max_zoom", 22),
             overlay=True,
             control=True,
         )
@@ -363,29 +334,22 @@ def xyz_to_plotly():
     """
     plotly_dict = {}
 
-    for key in xyz_tiles:
-        url = xyz_tiles[key]["url"]
-        attribution = xyz_tiles[key]["attribution"]
+    for key, tile in xyz_tiles.items():
         plotly_dict[key] = {
             "below": "traces",
             "sourcetype": "raster",
-            "sourceattribution": attribution,
-            "source": [url],
+            "sourceattribution": tile["attribution"],
+            "source": [tile["url"]],
             "name": key,
         }
-
-    xyz_dict = get_xyz_dict()
-    for item in xyz_dict:
-        name = xyz_dict[item].name
-        url = xyz_dict[item].build_url()
-        attribution = xyz_dict[item].attribution
-
-        plotly_dict[name] = {
+        
+    for item in get_xyz_dict().values():
+        plotly_dict[item.name] = {
             "below": "traces",
             "sourcetype": "raster",
-            "sourceattribution": attribution,
-            "source": [url],
-            "name": name,
+            "sourceattribution": item.attribution,
+            "source": [item.build_url()],
+            "name": item.name,
         }
 
     return plotly_dict
@@ -462,28 +426,22 @@ def xyz_to_heremap():
 
     heremap_dict = {}
 
-    for key in xyz_tiles:
-        name = xyz_tiles[key]["name"]
-        url = xyz_tiles[key]["url"]
-        attribution = xyz_tiles[key]["attribution"]
+    for key, tile in xyz_tiles.items():
         heremap_dict[key] = here_map_widget.TileLayer(
             provider=here_map_widget.ImageTileProvider(
-                url=url, attribution=attribution, name=name
+                url=tile["url"], 
+                attribution=tile["attribution"], 
+                name=tile["name"]
             )
         )
-
-    xyz_dict = get_xyz_dict()
-    for item in xyz_dict:
-        name = xyz_dict[item].name
-        url = xyz_dict[item].build_url()
-        attribution = xyz_dict[item].attribution
-        if "max_zoom" in xyz_dict[item].keys():
-            max_zoom = xyz_dict[item]["max_zoom"]
-        else:
-            max_zoom = 22
-        heremap_dict[name] = here_map_widget.TileLayer(
+    
+    for item in get_xyz_dict().values():
+        heremap_dict[item.name] = here_map_widget.TileLayer(
             provider=here_map_widget.ImageTileProvider(
-                url=url, attribution=attribution, name=name, max_zoom=max_zoom
+                url=item.build_url(),
+                attribution=item.attribution, 
+                name=item.name, 
+                max_zoom=item.pop("max_zoom", 22)
             )
         )
 
