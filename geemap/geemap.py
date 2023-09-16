07001a1e5a2b7d976bdc47b928aa08b99e851152
Qiusheng Wu <giswqs@gmail.com>
2023-09-16 12:57:29 -0400

    Set default add_header to False for add_widget

--- geemap/geemap.py
+++ geemap/geemap.py
@@ -6155,7 +6155,7 @@ class Map(core.Map):
         self,
         content,
         position="bottomright",
-        add_header=True,
+        add_header=False,
         opened=True,
         show_close_button=True,
         widget_icon="gear",
@@ -6170,7 +6170,7 @@ class Map(core.Map):
         Args:
             content (str | ipywidgets.Widget | object): The widget to add.
             position (str, optional): The position of the widget. Defaults to "bottomright".
-            add_header (bool, optional): Whether to add a header with close buttons to the widget. Defaults to True.
+            add_header (bool, optional): Whether to add a header with close buttons to the widget. Defaults to False.
             opened (bool, optional): Whether to open the toolbar. Defaults to True.
             show_close_button (bool, optional): Whether to show the close button. Defaults to True.
             widget_icon (str, optional): The icon name for the toolbar button. Defaults to 'gear'.
