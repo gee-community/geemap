00380dd6d381bd91d61fc9398da9d2bbfd7db62f
Qiusheng Wu <giswqs@gmail.com>
2023-10-17 21:05:32 -0400

    Change ValueError to TypeError

--- geemap/map_widgets.py
+++ geemap/map_widgets.py
@@ -142,23 +142,23 @@ class Colorbar(ipywidgets.Output):
         vmin = vis_params.get("min", kwargs.pop("vmin", 0))
         try:
             vmin = float(vmin) 
-        except ValueError as err:
+        except TypeError as err:
             msg = f'The provided data type of min has to be convertible to float. Instead it is {type(vmin)}.\n'
-            raise ValueError(msg + format_tb(err.__traceback__)[0] + err.args[0] + "\n") from None
+            raise TypeError(msg + format_tb(err.__traceback__)[0] + err.args[0] + "\n") from None
 
         vmax = vis_params.get("max", kwargs.pop("mvax", 1))
         try:
             vmax = float(vmax) 
-        except ValueError as err:
+        except TypeError as err:
             msg = f'The provided data type of max has to be convertible to float. Instead it is {type(vmax)}.\n'
-            raise ValueError(msg + format_tb(err.__traceback__)[0] + err.args[0] + "\n") from None
+            raise TypeError(msg + format_tb(err.__traceback__)[0] + err.args[0] + "\n") from None
 
         alpha = vis_params.get("opacity", kwargs.pop("alpha", 1))
         try:
             alpha = float(alpha)
-        except ValueError as err:
+        except TypeError as err:
             msg = f'The provided data type of opacity has to be convertible to float. Instead it is {type(alpha)}.\n'
-            raise ValueError(msg + format_tb(err.__traceback__)[0] + err.args[0] + "\n") from None
+            raise TypeError(msg + format_tb(err.__traceback__)[0] + err.args[0] + "\n") from None
 
         if "palette" in vis_params.keys():
             hexcodes = common.to_hex_colors(common.check_cmap(vis_params["palette"]))
