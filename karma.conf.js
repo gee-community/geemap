c31d5a9df9b571275b444119a5a86ddaf1214611
Sufyan Abbasi <sufy@google.com>
2024-10-15 01:52:19 +0000

    Set up tests for layer_manager_row

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: [
+      'jasmine',
+      'karma-typescript',
+    ],
+    files: [
+        { pattern: 'tests/**/*.spec.ts', watched: true }
+    ],
+    preprocessors: {
+        "**/*.ts": "karma-typescript",
+    },
+    plugins: [
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-jasmine-html-reporter',
+      'karma-typescript',
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        "helpers": ["./node_modules/jasmine-expect/index.js"],
+        "stopSpecOnExpectationFailure": false,
+        "random": true
+      }
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false,
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox'],
+      },
+    },
+  });
+};
