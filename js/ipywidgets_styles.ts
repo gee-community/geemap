b79ea78f4bcda3495973df6eebafa6d17e3df390
sufyanAbbasi <sufy.abbasi@gmail.com>
2024-11-11 10:06:32 -0800

    clean up styles

--- js/ipywidgets_styles.ts
+++ js/ipywidgets_styles.ts
@@ -23,9 +23,10 @@ export const legacyStyles = css`
 
     .legacy-button:hover:enabled,
     .legacy-button:focus:enabled {
-        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, var(--md-shadow-key-penumbra-opacity)),
-                    0 3px 1px -2px rgba(0, 0, 0, var(--md-shadow-key-umbra-opacity)),
-                    0 1px 5px 0 rgba(0, 0, 0, var(--md-shadow-ambient-shadow-opacity));
+        box-shadow: 0 2px 2px 0
+                rgba(0, 0, 0, var(--md-shadow-key-penumbra-opacity)),
+            0 3px 1px -2px rgba(0, 0, 0, var(--md-shadow-key-umbra-opacity)),
+            0 1px 5px 0 rgba(0, 0, 0, var(--md-shadow-ambient-shadow-opacity));
     }
 
     .legacy-slider {
