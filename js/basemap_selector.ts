41afc678266b72b112f37741c6caa252bd61775c
sufyanAbbasi <sufy.abbasi@gmail.com>
2024-11-11 18:03:35 -0800

    Add a rough basemap-selector widget

---
+++ js/basemap_selector.ts
@@ -0,0 +1,94 @@
+import type { RenderProps } from "@anywidget/types";
+import { css, html, TemplateResult } from "lit";
+import { property } from "lit/decorators.js";
+
+import { legacyStyles } from "./ipywidgets_styles";
+import { materialStyles } from "./styles";
+import { loadFonts } from "./utils";
+import { LitWidget } from "./lit_widget";
+
+export interface BasemapSelectorModel {
+    basemaps: string[];
+    value: string;
+}
+
+export class BasemapSelector extends LitWidget<
+    BasemapSelectorModel,
+    BasemapSelector
+> {
+    static get componentName() {
+        return `basemap-selector`;
+    }
+
+    static styles = [
+        legacyStyles,
+        materialStyles,
+        css`
+            .row {
+                align-items: center;
+                display: flex;
+                gap: 4px;
+                height: 30px;
+            }
+
+            .row-button {
+                font-size: 14px;
+                height: 26px;
+                width: 26px;
+            }
+        `,
+    ];
+
+    modelNameToViewName(): Map<
+        keyof BasemapSelectorModel,
+        keyof BasemapSelector
+    > {
+        return new Map([
+            ["basemaps", "basemaps"],
+            ["value", "value"],
+        ]);
+    }
+
+    @property({ type: Array }) basemaps: string[] = [];
+    @property({ type: String }) value: string = "";
+
+    render(): TemplateResult {
+        return html`
+            <div class="row">
+                <select @change=${this.onBaseMapChanged}>
+                    ${this.basemaps.map((basemap) => html`<option>${basemap}</option>`)}
+                </select>
+                <button
+                    class="legacy-button row-button close-button"
+                    @click="${this.onCloseClicked}"
+                >
+                    <span class="close-icon material-symbols-outlined">&#xe5cd;</span>
+                </button>
+            </div>`;
+    }
+
+    private onBaseMapChanged(event: Event) {
+        const target = event.target as HTMLInputElement;
+        this.value = target.value;
+    }
+
+    private onCloseClicked(_: Event) {
+        this.model?.send({ type: "click", id: "close" });
+    }
+}
+
+// Without this check, there's a component registry issue when developing locally.
+if (!customElements.get(BasemapSelector.componentName)) {
+    customElements.define(BasemapSelector.componentName, BasemapSelector);
+}
+
+function render({ model, el }: RenderProps<BasemapSelectorModel>) {
+    loadFonts();
+    const row = <BasemapSelector>(
+        document.createElement(BasemapSelector.componentName)
+    );
+    row.model = model;
+    el.appendChild(row);
+}
+
+export default { render };
