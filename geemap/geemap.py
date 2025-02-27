8ebe03939a9148c726635dd42b043c017cc14401
Fede Bello <fe.debello13@gmail.com>
2025-02-27 13:29:46 -0300

    Add max_error parameter to center_object method (#2228)
    * chore: add max error param to center object in foliumap backend
    
    * chore: add max error param to center object in core backend

--- geemap/geemap.py
+++ geemap/geemap.py
@@ -474,7 +474,10 @@ class Map(core.Map):
     setCenter = set_center
 
     def center_object(
-        self, ee_object: Union[ee.Element, ee.Geometry], zoom: Optional[int] = None
+        self,
+        ee_object: Union[ee.Element, ee.Geometry],
+        zoom: Optional[int] = None,
+        max_error: float = 0.001,
     ) -> None:
         """Centers the map view on a given object.
 
@@ -482,8 +485,9 @@ class Map(core.Map):
             ee_object (Union[ee.Element, ee.Geometry]): An Earth Engine object to
                 center on a geometry, image or feature.
             zoom (Optional[int], optional): The zoom level, from 1 to 24. Defaults to None.
+            max_error (float, optional): The maximum error for the geometry. Defaults to 0.001.
         """
-        super().center_object(ee_object, zoom)
+        super().center_object(ee_object=ee_object, zoom=zoom, max_error=max_error)
         if is_arcpy():
             bds = self.bounds
             arc_zoom_to_extent(bds[0][1], bds[0][0], bds[1][1], bds[1][0])
