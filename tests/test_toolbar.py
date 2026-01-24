e48435575f72e8be20ffe9dc023aa4756196e385
Kurt Schwehr <schwehr@google.com>
2026-01-24 06:59:43 -0800

    test_toolbar.py: Cleanup
    * No longer need the mock import
    * Do a module import of toolbar
    * Add type annotations for the TestToolbar class members
    * Rename `map` to `a_map` to avoid redefining a builtin
    * Rename `toolbar` instances to `a_toolbar` to avoid using the module name
    * Disable pylint protected-access for `_cleanup_toolbar_item`

--- tests/test_toolbar.py
+++ tests/test_toolbar.py
@@ -2,24 +2,29 @@
 """Tests for `map_widgets` module."""
 import dataclasses
 import unittest
-from unittest import mock
 
 import geemap
-from geemap.toolbar import Toolbar, ToolbarItem, _cleanup_toolbar_item
+from geemap import toolbar
 
 
 class TestToolbar(unittest.TestCase):
     """Tests for the Toolbar class in the `toolbar` module."""
+
+    callback_calls: int
+    last_called_with_selected: bool
+    last_called_item: toolbar.ToolbarItem
+    item: toolbar.ToolbarItem
+    reset_item: toolbar.ToolbarItem
 
     def setUp(self):
         super().setUp()
         self.callback_calls = 0
         self.last_called_with_selected = None
         self.last_called_item = None
-        self.item = ToolbarItem(
+        self.item = toolbar.ToolbarItem(
             icon="info", tooltip="dummy item", callback=self.dummy_callback
         )
-        self.reset_item = ToolbarItem(
+        self.reset_item = toolbar.ToolbarItem(
             icon="question",
             tooltip="no reset item",
             callback=self.dummy_callback,
@@ -33,31 +38,32 @@ class TestToolbar(unittest.TestCase):
         self.callback_calls += 1
 
     def test_no_tools_throws(self):
-        map = geemap.Map(ee_initialize=False)
-        self.assertRaises(ValueError, Toolbar, map, [], [])
+        a_map = geemap.Map(ee_initialize=False)
+        self.assertRaises(ValueError, toolbar.Toolbar, a_map, [], [])
 
     def test_only_main_tools_exist_if_no_extra_tools(self):
-        map = geemap.Map(ee_initialize=False)
-        toolbar = Toolbar(map, [self.item], [])
-        self.assertNotIn(toolbar.toggle_widget, toolbar.main_tools)
+        a_map = geemap.Map(ee_initialize=False)
+        a_toolboor = toolbar.Toolbar(a_map, [self.item], [])
+        self.assertNotIn(a_toolboor.toggle_widget, a_toolboor.main_tools)
 
     def test_all_tools_and_toggle_exist_if_extra_tools(self):
-        map = geemap.Map(ee_initialize=False)
-        toolbar = Toolbar(map, [self.item], [self.reset_item])
-        self.assertIsNotNone(toolbar.toggle_widget)
+        a_map = geemap.Map(ee_initialize=False)
+        a_toolboor = toolbar.Toolbar(a_map, [self.item], [self.reset_item])
+        self.assertIsNotNone(a_toolboor.toggle_widget)
 
     def test_toggle_expands_and_collapses(self):
-        map = geemap.Map(ee_initialize=False)
-        toolbar = Toolbar(map, [self.item], [self.reset_item])
-        self.assertIsNotNone(toolbar.toggle_widget)
-        toggle = toolbar.toggle_widget
+        """Toggle widget correctly expands and collapses the toolbar."""
+        a_map = geemap.Map(ee_initialize=False)
+        a_toolboor = toolbar.Toolbar(a_map, [self.item], [self.reset_item])
+        self.assertIsNotNone(a_toolboor.toggle_widget)
+        toggle = a_toolboor.toggle_widget
         self.assertEqual(toggle.icon, "add")
         self.assertEqual(toggle.tooltip_text, "Expand toolbar")
-        self.assertFalse(toolbar.expanded)
+        self.assertFalse(a_toolboor.expanded)
 
         # Expand
         toggle.active = True
-        self.assertTrue(toolbar.expanded)
+        self.assertTrue(a_toolboor.expanded)
         self.assertEqual(toggle.icon, "remove")
         self.assertEqual(toggle.tooltip_text, "Collapse toolbar")
         # After expanding, button is unselected.
@@ -65,30 +71,32 @@ class TestToolbar(unittest.TestCase):
 
         # Collapse
         toggle.active = True
-        self.assertFalse(toolbar.expanded)
+        self.assertFalse(a_toolboor.expanded)
         self.assertEqual(toggle.icon, "add")
         self.assertEqual(toggle.tooltip_text, "Expand toolbar")
         # After collapsing, button is unselected.
         self.assertFalse(toggle.active)
 
     def test_triggers_callbacks(self):
-        map = geemap.Map(ee_initialize=False)
-        toolbar = Toolbar(map, [self.item, self.reset_item], [])
+        """Tests that toolbar item callbacks are triggered correctly."""
+        a_map = geemap.Map(ee_initialize=False)
+        a_toolboor = toolbar.Toolbar(a_map, [self.item, self.reset_item], [])
         self.assertIsNone(self.last_called_with_selected)
         self.assertIsNone(self.last_called_item)
 
         # Select first tool, which does not reset.
-        toolbar.main_tools[0].active = True
+        a_toolboor.main_tools[0].active = True
         self.assertTrue(self.last_called_with_selected)
         self.assertEqual(self.callback_calls, 1)
-        self.assertTrue(toolbar.main_tools[0].active)
+        self.assertTrue(a_toolboor.main_tools[0].active)
         self.assertEqual(self.item, self.last_called_item)
 
         # Select second tool, which resets.
-        toolbar.main_tools[1].active = True
-        self.assertFalse(self.last_called_with_selected)  # was reset by callback
+        a_toolboor.main_tools[1].active = True
+        # Was reset by callback.
+        self.assertFalse(self.last_called_with_selected)
         self.assertEqual(self.callback_calls, 3)
-        self.assertFalse(toolbar.main_tools[1].active)
+        self.assertFalse(a_toolboor.main_tools[1].active)
         self.assertEqual(self.reset_item, self.last_called_item)
 
     @dataclasses.dataclass
@@ -100,34 +108,37 @@ class TestToolbar(unittest.TestCase):
             self.cleanup_count += 1
 
     def test_cleanup_toolbar_item_decorator(self):
+        """Tests the _cleanup_toolbar_item decorator functionality."""
         widget = TestToolbar.TestWidget()
 
-        @_cleanup_toolbar_item
+        # pylint: disable-next=protected-access
+        @toolbar._cleanup_toolbar_item
         def callback(m, selected, item):
+            del m, selected, item  # Unused.
             widget.selected_count += 1
             return widget
 
-        item = ToolbarItem(
+        item = toolbar.ToolbarItem(
             icon="info", tooltip="dummy item", callback=callback, reset=False
         )
         a_map = geemap.Map(ee_initialize=False)
-        toolbar = Toolbar(a_map, [item], [])
-        toolbar.main_tools[0].active = True
+        a_toolboor = toolbar.Toolbar(a_map, [item], [])
+        a_toolboor.main_tools[0].active = True
         self.assertEqual(1, widget.selected_count)
         self.assertEqual(0, widget.cleanup_count)
 
-        toolbar.main_tools[0].active = False
+        a_toolboor.main_tools[0].active = False
         self.assertEqual(1, widget.selected_count)
         self.assertEqual(1, widget.cleanup_count)
 
-        toolbar.main_tools[0].active = True
+        a_toolboor.main_tools[0].active = True
         self.assertEqual(2, widget.selected_count)
         self.assertEqual(1, widget.cleanup_count)
 
         widget.cleanup()
         self.assertEqual(2, widget.selected_count)
         self.assertEqual(3, widget.cleanup_count)
-        self.assertFalse(toolbar.main_tools[0].active)
+        self.assertFalse(a_toolboor.main_tools[0].active)
 
 
 if __name__ == "__main__":
